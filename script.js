12b2d84d415a727cae463c4765898f96eb3ebae3
Khanh <kirellia719@gmail.com>
2025-06-03 17:40:07 +0700

    add be

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const LIST_SERVER = [`http://localhost:8800`];
+const LIST_SERVER = [`https://2025-2026-score-06-be.vercel.app`];
 
 const searchBtn = document.getElementById("search-btn");
 const formUI = document.getElementById("form");
