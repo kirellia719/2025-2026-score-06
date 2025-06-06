17ab6f77dfb785407f97bf22900ed6cb3770cfe9
Khánh <kirellia719@gmail.com>
2025-06-06 08:51:18 +0700

    update phúc khảo

--- script.js
+++ script.js
@@ -5,41 +5,41 @@ const formUI = document.getElementById("form");
 const wrapper = document.getElementById("wrapper");
 
 const MRound = (n) => {
-  return Math.round(n * 1000) / 1000;
+   return Math.round(n * 1000) / 1000;
 };
 
 const DiemChuan = 16.49;
 
 const ribbon = (result) => {
-  if (result) {
-    return `<div class="fancy-ribbon pass">
+   if (result) {
+      return `<div class="fancy-ribbon pass">
                <span>🏅 Trúng tuyển</span>
             </div>`
-  }
-  else return `<div class="fancy-ribbon fall">
+   }
+   else return `<div class="fancy-ribbon fall">
                <span>Không trúng tuyển</span>
             </div>`
 }
 
 const noti = (result) => {
-  if (result) {
-    return `<i class="noti">
+   if (result) {
+      return `<i class="noti">
                Xin chúc mừng thí sinh. <br/>
                Phụ huynh vui lòng nộp hồ sơ từ ngày 6/6/2025 đến 8/6/2025 nhé
             </i>`
-  }
-  else return ` <i class="noti">~ Rất tiếc, thí sinh chưa trúng tuyển. ~ </i>`
+   }
+   else return ` <i class="noti">~ Rất tiếc, thí sinh chưa trúng tuyển. ~ </i>`
 }
 
 const showProfile = (info) => {
-  const { math = 0, english = 0, literature = 0, extra = 0, SBD = "", fullname = "", born = "" } = info;
+   const { math = 0, english = 0, literature = 0, extra = 0, SBD = "", fullname = "", born = "", checked = 0 } = info;
 
-  const isAbsence = !math && !english && !literature;
+   const isAbsence = !math && !english && !literature;
 
-  const sum = MRound(extra + math + english + literature);
+   const sum = MRound(extra + math + english + literature);
 
-  const result = sum >= DiemChuan;
-  const profile = `
+   const result = sum >= DiemChuan;
+   const profile = `
     <div class="card">
             <div class="information">
                <div class="avatar">
@@ -77,43 +77,47 @@ const showProfile = (info) => {
                <hr>
                <div class="sum">
                   <span class="sum-title">Tổng điểm ${extra ? `<span class="extra">+ ${extra}</span>` : ''} </span>
-                  <span class="sum-score">${MRound(sum)}</span>
+                  <span class="sum-score ${checked ? "line-through" : ""}">${MRound(sum)}</span>
                </div>
+               ${checked ? `<div class="sum">
+                  <span class="sum-title">Sau phúc khảo </span>
+                  <span class="sum-score">${MRound(checked)}</span>
+               </div>` : ""}
                <hr>
             </div>
             ${result != null ? ribbon(result) : ''}
             ${result != null ? noti(result) : ''}
          </div>
   `;
-  return profile;
+   return profile;
 };
 
 const loader = `<div><div class="loader"></div></div>`;
 
 const hiddenScore = () => {
-  const content = `<div>Chưa có kết quả</div>`;
-  wrapper.innerHTML = content;
+   const content = `<div>Chưa có kết quả</div>`;
+   wrapper.innerHTML = content;
 };
 
 const searchAction = async (e) => {
-  e.preventDefault();
-  let SBDInput = document.getElementById("SBD-input").value;
-  SBDInput = SBDInput.trim();
-  if (SBDInput) {
-    // hiddenScore();
-    wrapper.innerHTML = loader;
-    let index = Math.floor(Math.random() * LIST_SERVER.length);
-    const SERVER_URL = LIST_SERVER[index];
-    fetch(`${SERVER_URL}/candidates/${SBDInput}`)
-      .then((r) => r.json())
-      .then((data) => {
-        wrapper.innerHTML = showProfile(data);
-      })
-      .catch(() => {
-        const error = `<div style="padding: 5px 10px; background-color: white; border-radius: 4px;">Không tìm thấy SBD <b>${SBDInput}</b></div>`;
-        wrapper.innerHTML = error;
-      });
-  }
+   e.preventDefault();
+   let SBDInput = document.getElementById("SBD-input").value;
+   SBDInput = SBDInput.trim();
+   if (SBDInput) {
+      // hiddenScore();
+      wrapper.innerHTML = loader;
+      let index = Math.floor(Math.random() * LIST_SERVER.length);
+      const SERVER_URL = LIST_SERVER[index];
+      fetch(`${SERVER_URL}/candidates/${SBDInput}`)
+         .then((r) => r.json())
+         .then((data) => {
+            wrapper.innerHTML = showProfile(data);
+         })
+         .catch(() => {
+            const error = `<div style="padding: 5px 10px; background-color: white; border-radius: 4px;">Không tìm thấy SBD <b>${SBDInput}</b></div>`;
+            wrapper.innerHTML = error;
+         });
+   }
 };
 
 searchBtn.addEventListener("click", searchAction);
